8abd812ebfd742c17629a35b95e016242df304ba
vandanafuloria <vandanafuloria02@gmail.com>
2025-06-17 14:37:28 +0530

    added favicon

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/CV-builder-/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["fsevents"], // ✅ Tell Vite not to optimize/bundle this
